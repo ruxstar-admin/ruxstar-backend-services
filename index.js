3191de8eab9694a1340890b153868ab03aa2b04a
Easwanth konduru <256533456+ruxstar-admin@users.noreply.github.com>
2026-05-26 19:00:50 +0530

    default Endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
 const express = require('express');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const db = require('./lib/db');
 const routes = require('./routes');
 
