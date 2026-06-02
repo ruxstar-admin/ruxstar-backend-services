5f45da04c7ac64866bd22a7e78802cf48a37c576
Varsha-Nizamkari12 <nizamkarivarsha2@gmail.com>
2026-06-02 16:29:01 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,9 @@ const authenticate = require('../middlewares/auth');
 
 const router = Router();
 
+
 router.post('/send-otp', authController.sendOtp);
+
 router.post('/verify-otp', authController.verifyOtp);
 router.get('/me', authenticate, authController.me);
 router.post('/logout', authenticate, authController.logout);
